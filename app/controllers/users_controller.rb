7a9c9e7387826d838946cef6bd38169218e7788f
Kirsten Schumy <kirsten.schumy@gmail.com>
2018-04-09 17:41:58 -0700

    added initial user controller methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
 class UsersController < ApplicationController
+
+	def index
+		@user = User.all
+	end
+
+
+	def show
+		@user = User.find_by(id: params[:id])
+	end
+
+	# Creates a new user
+	def create
+		User.create(user_params)
+		redirect_to users_path
+	end
+
+	private
+
+	def user_params
+    return params.require(:user).permit(:id, :name)
+	end
+
 end
