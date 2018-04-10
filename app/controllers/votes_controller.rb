92a2deda7c5774574b939cda1d83306ead4076f9
Kirsten Schumy <kirsten.schumy@gmail.com>
2018-04-09 17:50:45 -0700

    added initial votes controller methods

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,2 +1,19 @@
 class VotesController < ApplicationController
+
+	def index
+		@vote = Vote.all
+	end
+
+	# Creates a new vote
+	def create
+		Vote.create(vote_params)
+		# redirect_to vote_path(params[:id])
+	end
+
+	private
+
+	def work_params
+		return params.require(:work).permit(:id, :user_id, :work_id)
+	end
+
 end
