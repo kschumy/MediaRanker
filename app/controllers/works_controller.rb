927e43a33b2d31c939799b2ebfbcf4fac70f7a75
Kirsten Schumy <kirsten.schumy@gmail.com>
2018-04-09 17:48:22 -0700

    added initial works controller methods

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,2 +1,30 @@
 class WorksController < ApplicationController
+
+	def index
+		@work = Work.all
+	end
+
+	def show
+		@work = Work.find_by(id: params[:id])
+	end
+
+	# Creates a new work
+	def create
+		Work.create(work_params)
+		redirect_to work_path(params[:id])
+	end
+
+	def edit
+		@work = Work.find_by(id: params[:id])
+		@work.update(work_params)
+	 	redirect_to work_path(params[:id])
+	end
+
+	private
+
+	def work_params
+		return params.require(:work).permit(:id, :title, :category, :publication_year,
+			:description, :creator)
+	end
+
 end
