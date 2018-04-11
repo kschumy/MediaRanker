811c6b27d741af13983c92dff35936d5917fe9cd
Kirsten Schumy <kirsten.schumy@gmail.com>
2018-04-10 18:26:41 -0700

    learned how to spell album and added addtional validation tests for work

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,5 +1,26 @@
 class Work < ApplicationRecord
 	has_many :votes
-	validates :title, uniqueness: { scope: :category,
-    message: "title: has already been taken" }
+
+	before_validation :squish_title_and_description
+
+	validates :title, :length => { minimum: 1 }, :uniqueness => {
+		:scope => :category, :case_sensitive => false }
+
+	validates :category, :inclusion => { :in => %w[movie book album] }
+
+	def get_vote_count
+		return calculate_vote_count
+	end
+
+	private
+
+	def calculate_vote_count
+		return votes.size
+	end
+
+	def squish_title_and_description
+		self.title = self.title.squish if !self.title.nil? # TODO: need? Validated above
+		self.description = self.description.squish if !self.description.nil?
+	end
+
 end
--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -1,6 +1,79 @@
 require "test_helper"
 
 describe Work do
+
+describe 'valid' do
+
+  it "must have a title" do
+    work = works(:hpbook)
+    work.title = nil
+    work.valid?.must_equal false
+  end
+
+  it "gets strips white space from title" do
+    valid_new_work = Work.create(title: "foo bar         baz", category: "book")
+    valid_new_work.valid?.must_equal true
+    valid_new_work.title.must_equal "foo bar baz"
+
+    invalid_new_work_one = Work.create(title: "foo    bar   baz", category: "book")
+    invalid_new_work_one.valid?.must_equal false
+
+    invalid_new_work_two = Work.create(title: "   foo bar baz", category: "book")
+    invalid_new_work_two.valid?.must_equal false
+  end
+
+  it "must have a title of at least one char" do
+    new_work = Work.create(title: "", category: "book")
+    new_work.valid?.must_equal false
+
+    new_work = Work.create(title: "         ", category: "book")
+    new_work.valid?.must_equal false
+
+    new_work = Work.create(title: "a", category: "book")
+    new_work.valid?.must_equal true
+  end
+
+  it "must have a unique title in same category" do
+    new_work = Work.create(title: works(:hpbook).title, category:
+      works(:hpbook).category)
+    new_work.valid?.must_equal false
+  end
+
+  it "must have a unique, case-insensitive title" do
+    new_work_title = works(:hpbook).title
+    new_work_title[0] = new_work_title.chr.swapcase
+    new_work = Work.create(title: new_work_title, category:
+      works(:hpbook).category)
+
+    new_work.valid?.must_equal false
+  end
+
+  it "allow duplicate title in different category" do
+    works(:hpbook).category.must_equal "book"
+    new_work = Work.create(title: works(:hpbook).title, category: "album")
+    new_work.valid?.must_equal true
+  end
+
+  it "does not allow an invalid category" do
+    work = works(:bundo)
+
+    work.category = "foo"
+    work.valid?.must_equal false
+
+    work.category = nil
+    work.valid?.must_equal false
+  end
+
+  it "allows a valid category" do
+    work = works(:bundo)
+    %w[movie book album].each do |category_type|
+      work.category = category_type
+      work.valid?.must_equal true
+    end
+  end
+
+end
+
   describe 'relations' do
 
    # it "has a votes" do
@@ -19,4 +92,98 @@ describe Work do
 
   end
 
+
+  describe 'get_vote_count' do
+
+   it 'responds to get_vote_count' do
+     work = works(:hpbook)
+     work.must_respond_to :get_vote_count
+     # test_vote = Vote.create work: works(:hpbook), user: users(:ada)
+     # work.votes.last.must_equal test_vote
+   end
+  end
+
 end
+
+
+#
+#
+# it "must have a title" do
+#   book.author = Author.create name: "Kari B"
+#
+#   book.title = nil
+#
+#   book.valid?.must_equal false
+#   book.errors.must_include :title
+#
+# end
+#
+# it "must have a title between 1 and 25 letters" do
+#   book.author = Author.create name: "Kari B"
+#
+#   book.title = ""
+#   book.valid?.must_equal false
+#
+#   book.title = "12345678901234567890123456"
+#   book.valid?.must_equal false
+#
+# end
+#
+# it "must have an author" do
+#   author =  Author.create name: "Kari B"
+#
+#   book.author = author
+#
+#   book.author_id.must_equal author.id
+#
+# end
+#
+# it "must have a genre field" do
+#   book.genres.must_equal []
+#   book.genres << Genre.create( name: "Sci Fi")
+#
+#   genre = Genre.find_by name: "Sci Fi"
+#
+#   book.genres.must_include genre
+#
+# end
+
+
+# describe "first published" do
+#   it "should return the earliest published book for the author with multiple books" do
+#     book.author = Author.create name: "Kari B"
+#     value(book).must_be :valid?
+#   end
+#
+#   it "must have as title" do
+#     book.author = Author.create name: "Kari B"
+#     book.title = nil
+#     book.valid?.must_equal false # must have a title
+#     book.errors.must_include :title
+#   end
+#
+#   it "must have title between 1 and 25 characters" do
+#     book.author = Author.create name: "Kari B"
+#     book.title = ""
+#     book.valid?.must_equal false
+#     book.errors.must_include :title
+#
+#     book.title = "a" * 26
+#     book.valid?.must_equal false
+#     book.errors.must_include :title
+#   end
+#
+#   it "must have an author" do
+#     author = Author.create name: "Kari B"
+#
+#     book.author = author
+#     book.author_id.must_equal author.id
+#   end
+#
+#   it "must have a genre field" do
+#     book.genres.must_equal []
+#     book.genres << Genre.create(name: "Sci Fi")
+#
+#     genre = Genre.find_by name: "Sci Fi"
+#     book.genres.must_include genre
+#   end
